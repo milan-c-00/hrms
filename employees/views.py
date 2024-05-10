07dcc2971f4576b9aacab85a4e8fcc35982d489e
tasa <tasa.kovac2001@gmail.com>
2024-05-10 13:13:43 +0200

    made some changes

--- employees/views.py
+++ employees/views.py
@@ -13,3 +13,41 @@ def delete_employee(request, employee_id):
         employee.delete()
         return redirect('/employees/')  
     return render(request, {'employee': employee})
+
+def detail(request, employee_id):
+    detailemployee=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/detail.html',{'employee':detailemployee})
+
+def personal(request, employee_id):
+    personalemployee=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/personal.html',{'personalemployee':personalemployee})
+
+def education(request, employee_id):
+    employee=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/education.html',{'employee':employee})
+
+def jobobjective(request, employee_id):
+    employee=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/jobobjective.html',{'employee':employee})
+
+def documents(request, employee_id):
+  #  documents=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/documents.html')
+
+def status(request, employee_id):
+    employee=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/status.html',{'employee':employee})
+
+def jobdetails(request, employee_id):
+  #  documents=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/jobdetails.html')
+
+def performance(request, employee_id):
+  #  documents=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/performance.html')
+
+def daysoff(request, employee_id):
+  #  documents=get_object_or_404(Employee, pk=employee_id)
+    return render(request,'employees/daysoff.html')
+
+
--- hrms/settings.py
+++ hrms/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hrms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['hrms/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
